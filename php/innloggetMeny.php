dbce2e3a599909a8abf59e077695d0116eb2daf4
Stainging Lamb 272 <ray.htc.legend@gmail.com>
2021-02-25 19:59:21 +0100

    tried func for innlogging_sjekk and menu

--- php/innloggetMeny.php
+++ php/innloggetMeny.php
@@ -5,12 +5,40 @@
 
 // $navn = "Logget inn som " . $_SESSION['navn'];
 $name = $_SESSION['navn'];
-$kandidate = $_SESSION['kandidat'];
+$kandidat = $_SESSION['kandidat'];
 $brukertype = $_SESSION['brukertype'];
+$nomineringsperiode = $_SESSION['nomineringsperiode'];
+$valgperiode = $_SESSION['valgperiode'];
 
+function brukerMeny() {
+	// meny for innlogget vanligbruker og ekstra hvis brukeren er kandidat
+	echo <<<MKR
+	<a href="default.php"> <img class="dtpics" src="images/election.png" alt="valgurne" height="100" width="130"> </a>
+	<button id="hamburgermeny" onclick=toggleMenu()>MENY</button>
+	<ul id="menuitems">
+	<li id="home"><a href="default.php">Home</a></li>
+	<li><a href="byttpassord.php">Bytt passord</a></li>
+	MKR;
+	echo "<li>Kandiat is " . $GLOBALS['kandidat']. $GLOBALS['brukertype'] ."</li>";
+	if($GLOBALS['nomineringsperiode']) { echo "<li><a href=\"nominering.php\">Nominering</a></li>";	}
+	if($GLOBALS['valgperiode']) { echo "<li><a href=\"avstemning.php\">Avstemning</a></li>";}
+	if($GLOBALS['kandidat']) { echo "<li><a href=\"myprofil.php\">Kandidatur</a></li>";	}
+}
 
-?> 
+function adminMeny() {
+	echo '<li><a href="valgadmin.php">Administere Valget</a></li>';
+	echo '<li><a href="valgadmin.php">Administere Valget SOPA</a></li>';
+}
 
+brukerMeny();
+if ($brukertype == 2) {
+	adminMeny();
+}
+
+echo "</ul> \n
+	<a href=\"php/loggut.php\" class=\"logginnBtn\">Logg ut  " .$GLOBALS['name']."</a>";
+
+/* 
 <!-- kode mellom nav taggene -->
 <a href="default.php"> <img class="dtpics" src="images/election.png" alt="valgurne" height="100" width="130"> </a>
 <button id="hamburgermeny" onclick=toggleMenu()>MENY</button>
@@ -21,10 +49,16 @@ $brukertype = $_SESSION['brukertype'];
 	<li><a href="byttpassord.php">Bytt passord</a></li>
 	<?php
 	if ($kandidate){ echo '<li><a href="myprofile.php">Kandidatur</a></li>'; }
-	if ($brukertype == 2) {echo '<li><a href="valgadmin.php">Administere Valget</a></li>';}
+	if ($brukertype == 2) {
+		echo '<li><a href="valgadmin.php">Administere Valget</a></li>';
+		echo '<li><a href="valgadmin.php">Administere Valget SOPA</a></li>';
+	}
 	?>
 	<!-- <li><?php echo $byttpassord ?>Bytt Passord</a></li>  ?? ungår bytting mellome html og php modus??? -->
 	<!--<li><?php echo $navn ?></li>-->
 	
 </ul>
 <a href="php/loggut.php" class="logginnBtn">Logg ut <?php echo " ".$name?></a>
+ */
+
+?>
--- php/innlogging_sjekk.php
+++ php/innlogging_sjekk.php
@@ -10,8 +10,49 @@ if(!$mydb) {
     exit("feil med forbindelse");
 }
 
+function isKandidat() {
+    echo "Call isKandidat";
+    // ###### sjekk om bruker er kandidat ############
+    $sql1 = "SELECT bruker FROM kandidat WHERE bruker = :bruker"; //AND avslått FALSE
+    $stm1 = $GLOBALS['mydb'] -> prepare($sql1);
+    $stm1 -> bindParam(":bruker", $bruker);
+    $stm1 -> execute();
+    $res = $stm1 -> fetch(PDO::FETCH_ASSOC);
+    if($stm1 -> rowCount() == 1) {
+        return TRUE;
+    } else {
+        return FALSE;
+    }
+}
+
+function isNominering() {
+    $sql = "SELECT startforslag, sluttforslag FROM valg";
+    return isGyldigPeriode($sql);
+}
+
+function isValg() {
+    $sql = "SELECT startvalg, sluttvalg FROM valg";
+    return isGyldigPeriode($sql);
+}
+
+function isGyldigPeriode($sql) {
+    echo "\nIsGyldigPeriode\n";
+    var_dump($sql);
+    $stm = $GLOBALS['mydb'] -> prepare($sql);
+    $stm -> execute();
+    $res = $stm -> fetch(PDO::FETCH_ASSOC);
+    print_r($res);
+    //Sjekk mot perioden mot dagens dato
+    /* if (date('Y-m-d') >= $res[0] && date('Y-m-d') <= $res[1]) {
+        return TRUE;
+    } else {
+        return FALSE;
+    } */
+
+}
+
 if(isset($_POST["logginn"])) { // knapp trykket fra logginn siden
-    echo "logginn knappen trykket ";
+    // echo "logginn knappen trykket ";
     $salt = "IT2_2021";
     $passord = $_POST["password"];
     $bruker = $_POST["username"];
@@ -29,31 +70,22 @@ if(isset($_POST["logginn"])) { // knapp trykket fra logginn siden
     
     if($stm -> rowCount() == 1) { //skjekk innloggingsinformasjon mot databasen
         
-        // ###### sjekk om bruker er kandidat ############
-        $sql1 = "SELECT bruker FROM kandidat WHERE bruker = :bruker";
-        $stm1 = $mydb -> prepare($sql1);
-        $stm1 -> bindParam(":bruker", $bruker);
-        $stm1 -> execute();
-        $res = $stm1 -> fetch(PDO::FETCH_ASSOC);
-        if($stm1 -> rowCount() == 1) {
-            $kandidat = TRUE;
-        } else {
-            $kandidat = FALSE;
-        }
-
+        
         $fulltnavn = $result['fnavn']." ".$result['enavn'];
         $brukertype = $result['brukertype']; // #### 17.feb sprint 4 tar være på bruketype og justere innloggetMeny.php
         $_SESSION['navn'] = $fulltnavn;
         $_SESSION['innlogget'] = TRUE;
         $_SESSION['epost'] = $bruker;
-        $_SESSION['kandidat'] = $kandidat;
         $_SESSION['brukertype'] = $brukertype; // #### Set i cookie
+        $_SESSION['kandidat'] = isKandidat();
+        $_SESSION['nomineringsperiode'] = isNominering();
+        $_SESSION['valgperiode'] = isValg();
         // echo '<script>alert("Logginn vellykket");</script>';
         // echo '<script>window.location.assign("../avsteming.php"</script>';
         // var_dump($kandidat);
         // echo "<br> res" . var_dump($res);
         // echo "brukertype:  $brukertype";
-        header("Location: ../avstemning.php");
+        //header("Location: ../avstemning.php");
         
     } else {
         // echo '<script>alert("Logginn mislykkes");</script>';
@@ -66,7 +98,5 @@ if(isset($_POST["logginn"])) { // knapp trykket fra logginn siden
     header("Location: ../logginn.html"); //skrevet siden i url
 }
 
-function skjekk_kandidatur($mydb) {
 
-}
 ?>
