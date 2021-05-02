2718b74a1490856e6aebd318443abc4d1762e221
StainingLamb272 <ray.htc.legend@gmail.com>
2021-05-02 14:02:59 +0200

    fixed utloggetMeny.php to just show regitering, logg-in & home

--- php/utloggetMeny.php
+++ php/utloggetMeny.php
@@ -1,15 +1,14 @@
 <?php
-// Siden utviklet av Raymond Dowling sist endret 8.november 2020
-?>
+// Siden utviklet av Raymond Dowling sist endret 30.april 2020
+// Bare 'Home' 'Registering' og 'Logg inn'
 
-<!-- kode mellom nav taggene -->
+echo <<<MKR
 <a href="default.php"> <img class="dtpics" src="images/election.png" alt="valgurne" height="100" width="130"> </a>
 <a href="logginn.html" class="logginnBtn">Logg inn</a>
 <button id="hamburgermeny" onclick=toggleMenu()>MENY</button>
     <ul id="menuitems">
 		<li id="home"><a href="default.php">Home</a></li>
-		<!-- <li><a href="nominering.html">Nominering</a></li>
-		<li><a href="avstemning.html">Avstemning</a></li> -->
 		<li><a href="registering.html">Registering</a></li>
 	</ul>
-	
+MKR;	
+?>
