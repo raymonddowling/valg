027992d157e39215bbc8d0e2c6a1da346e39215b
StainingLamb272 <56690909+StainingLamb272@users.noreply.github.com>
2021-10-01 20:11:37 +0200

    Update dbconnect.php

--- php/dbconnect.php
+++ php/dbconnect.php
@@ -4,7 +4,6 @@ class mypdo extends PDO {
     public function __construct() {
         $drv = 'mysql';
         $host = "localhost";
-        // $host = "S381.usn.no"; //** ping gir 32.134 "128.39.19.159"; **
         $dbname = "valg2021";
         $usr = "usr_valg";
         $pwrd = "pw_valg2021";
